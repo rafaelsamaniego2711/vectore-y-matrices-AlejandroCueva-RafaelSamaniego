84e718f15824c46f81c0061bce436cec3d730533
Rafael Nicolás Samaniego Gancino <rafael.samaniego@udla.edu.ec>
2026-04-15 22:33:44 -0500

    Creacion de menu y definicion de maximos y minimos

--- main.c
+++ main.c
@@ -8,3 +8,19 @@ int main () {
     float maxest[5], minest[5];
     int aprobados[3] = {0, 0, 0};
     int reprobados[3] = {0, 0, 0};
+
+    printf ("--------Sistema de calificaciones--------\n");
+
+    for (int j = 0; j < 3; j++) {
+        printf ("\nSeleccione la materia deseada: ");
+        switch (j) {
+            case 0: printf ("matematica\n"); break;
+            case 1: printf ("lenguaje\n"); break;
+            case 2: printf ("computacion\n"); break;
+
+            float sumacol = 0;
+            maxasig[j] = -1;
+            minasig[j] = 11;
+
+    
+
