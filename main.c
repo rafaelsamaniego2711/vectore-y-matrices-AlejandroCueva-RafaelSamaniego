5cc565900d923154c9f986189f2b7a3018660981
Rafael Nicolás Samaniego Gancino <rafael.samaniego@udla.edu.ec>
2026-04-15 22:10:09 -0500

    Declaracion de variables

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
 
 int main () {
+
+    float notas[5][3];
+    float promestudiantes[5], promasignaturas[3];
+    float maxasig[3], minasig[3];
+    float maxest[5], minest[5];
+    int aprobados[3] = {0, 0, 0};
+    int reprobados[3] = {0, 0, 0};
